89bd821c178543450e4e7a2a5b96bc4edddd0c4e
Satyam <tsingh30399@gmail.com>
2023-11-22 15:02:19 +0530

    added : expense chart code

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import ExpenseItem  from './components/ExpenseItem';
+
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h2>lets get started!</h2>
+      <ExpenseItem></ExpenseItem>
+    </div>
+  );
+}
+
+export default App
