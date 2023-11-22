fa7ac4a31659044fc1bc989f041b75e9c53573ba
Satyam <tsingh30399@gmail.com>
2023-11-22 20:26:36 +0530

    added : expense chart code

--- src/App.jsx
+++ src/App.jsx
@@ -6,10 +6,40 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const expenses = [
+    {tittle: 'Car Insurance', 
+    amount: '100', 
+    date: new Date(2023, 9, 10)
+  },
+    {tittle: 'Car', 
+    amount: '1000', 
+    date: new Date(2023, 9, 10)},
+    {tittle: 'Insurance', 
+    amount: '120', 
+    date: new Date(2023, 9, 10)},
+    {tittle: 'Grocery', 
+    amount: '300', 
+    date: new Date(2023, 9, 10)}
+  ];
   return (
     <div>
       <h2>lets get started!</h2>
-      <ExpenseItem></ExpenseItem>
+      <ExpenseItem tittle = {expenses[0].tittle}
+      amount ={expenses[0].amount}
+      date ={expenses[0].date}
+      ></ExpenseItem>
+      <ExpenseItem tittle = {expenses[1].tittle}
+      amount ={expenses[1].amount}
+      date ={expenses[1].date}
+      ></ExpenseItem>
+      <ExpenseItem tittle = {expenses[2].tittle}
+      amount ={expenses[2].amount}
+      date ={expenses[2].date}
+      ></ExpenseItem>
+      <ExpenseItem tittle = {expenses[3].tittle}
+      amount ={expenses[3].amount}
+      date ={expenses[3].date}
+      ></ExpenseItem>
     </div>
   );
 }
--- src/components/ExpenseItem.jsx
+++ src/components/ExpenseItem.jsx
@@ -1,15 +1,14 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css'
 
-function ExpenseItem(){
-    const expenseDate = new Date(2023, 9, 10);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 100; 
+function ExpenseItem(props){ 
+    
     return (
         <div className='expense-item'>
-            <div>{expenseDate.toISOString()}</div>
+           <ExpenseDate date = {props.date}></ExpenseDate>
             <div className='expense-item__description'>
-                <h2>{expenseTitle}</h2>
-                <div className='expense-item__price'>${expenseAmount}</div>
+                <h2>{props.tittle}</h2>
+                <div className='expense-item__price'>${props.amount}</div>
             </div>
         </div>
     );
